a1c57df43ab4579cf67b15977ee09a608af7d430
akilm <akilmv555@gmail.com>
2020-10-03 23:31:46 +0530

    logic effort delay func

--- Cin_tot.m
+++ Cin_tot.m
@@ -1,10 +1,11 @@
-function C_Table = Cin_tot(No_inputs,Gate_Type,Width,gamma)
+function Cin_arr = Cin_tot(No_inputs,Gate_Type,Width,gamma)
     switch Gate_Type
         case 'INV'
-            C_Table = (gamma+1)*Width
+            Cin_arr = (gamma+1)*Width
         case 'NAND'
-            C_Table = (gamma+No_inputs)*Width
+            Cin_arr = (gamma+No_inputs)*Width
         case 'NOR'
-            C_Table = (No_inputs*gamma+1)*Width
+            Cin_arr = (No_inputs*gamma+1)*Width
     end
+    
 end
--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 LOGIC_String = input('Enter the LOGIC String \nSpecified Format: <No_Inputs>-<Gate_Type>-<No_Branches> <No_Inputs>-<Gate_Type>-<No_Branches>  \nSample Formats : 4-NAND-3 1-INV-2 5-NOR-3 \n ')
-Stages = split(LOGIC_String)
+gamma = input('Enter the Gamma Value')
 Cout = input('enter the Load Capacitance in pf')
-
+Cin = CIN(LOGIC_String,Width,gamma,Cout)
 
 end
