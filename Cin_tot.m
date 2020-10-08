48c13c12973c34808d875cddf7954fa1dbf47154
akilm <akilmv555@gmail.com>
2020-10-08 22:23:12 +0530

    Updated Capacitance Model

--- Cin_tot.m
+++ Cin_tot.m
@@ -1,11 +1,14 @@
-function Cin_arr = Cin_tot(No_inputs,Gate_Type,Width,gamma)
+function [Cin_arr,Cout_arr] = Cin_tot(No_inputs,Gate_Type,Width,gamma,Cg,Cd)
     switch Gate_Type
         case 'INV'
-            Cin_arr = (gamma+1)*Width
+            Cin_arr  = (gamma+1)*Width(i)*Cg
+            Cout_arr = (gamma+1)*Width(i)*Cd 
         case 'NAND'
-            Cin_arr = (gamma+No_inputs)*Width
+            Cin_arr = (gamma+No_inputs)*Width(i)*Cg
+            Cout_arr = (No_inputs*gamma + No_inputs)*Cd
         case 'NOR'
-            Cin_arr = (No_inputs*gamma+1)*Width
+            Cin_arr = (No_inputs*gamma+1)*Width(i)*Cg
+            Cout_arr = (No_inputs*gamma + No_inputs)*Cd
     end
     
 end
--- effort_delay.m
+++ effort_delay.m
@@ -1,6 +1,7 @@
-function [d,C_out,Gate_Type,No_inputs] = d(LOGIC_String,Width,gamma,C_L,Pinv)
+function [d,C_out,C_in,uuGate_Type,No_inputs] = d(LOGIC_String,Width,gamma,C_L,Pinv)
     stage = split(LOGIC_String)
     C_in = zeros(1,length(stage))
+    C_out_temp = zeros(1,length(stage))
     C_out = zeros(1,length(stage))
     g = zeros(1,length(stage))
     h = zeros(1,length(stage))
@@ -11,7 +12,7 @@ function [d,C_out,Gate_Type,No_inputs] = d(LOGIC_String,Width,gamma,C_L,Pinv)
        No_inputs(i) = str2double(cell2mat(str(1)))
        Gate_Type(i) = string(str(2))
        No_Branches(i) = cell2mat(str(3))
-       C_in(i) =  Cin_tot(No_inputs(i),Gate_Type(i),Width(i),gamma)
+       [C_in(i),C_out_temp] =  Cin_tot(No_inputs(i),Gate_Type(i),Width(i),gamma,Cg,Cd)
        g(i) = logic_effort(Gate_Type(i),No_inputs(i),gamma)
        p(i) = parasitic_delay(Gate_Type(i),No_inputs(i),1)
     end
@@ -24,6 +25,7 @@ function [d,C_out,Gate_Type,No_inputs] = d(LOGIC_String,Width,gamma,C_L,Pinv)
     end
     for i = 1:length(stage)
         h(i) = C_out(i)/C_in(i)
+        C_out(i) = C_out(i)+C_out_temp(i)
     end
     d = 0
     for i = 1:length(stage)
